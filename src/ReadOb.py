bbf79ae1c7d71628aec9829bc0387b7162576fb0
Nikki Tebaldi <ntebaldi@umass.edu>
2021-08-30 14:45:30 -0400

    Retrieve missing as NaN and 'nt'

--- src/ReadOb.py
+++ src/ReadOb.py
@@ -4,15 +4,15 @@ Read reach data from SWOT rivertile file.
 import netCDF4 as nc
 import numpy as np
 
-def Rivertile(rivertile_dir, rch):
+def Rivertile(swot_file):
     
-    rivertile_path = rivertile_dir + str(rch) + '_SWOT.nc'
-    dataset = nc.Dataset(rivertile_path, 'r')
+    dataset = nc.Dataset(swot_file, 'r')
     rivertile = {}
-    rivertile['reach_id'] = np.array(dataset['reach']['reach_id'][:])
-    rivertile['height'] = np.array(dataset['reach']['wse'][:])
-    rivertile['width'] = np.array(dataset['reach']['width'][:])
-    rivertile['slope'] = np.array(dataset['reach']['slope2'][:])
+    rivertile['reach_id'] = dataset['reach']['reach_id'][:].filled(np.nan)
+    rivertile['height'] = dataset['reach']['wse'][:].filled(np.nan)
+    rivertile['width'] = dataset['reach']['width'][:].filled(np.nan)
+    rivertile['slope'] = dataset['reach']['slope2'][:].filled(np.nan)
+    rivertile['nt'] = dataset.dimensions["nt"].size
  
     dataset.close()
     return rivertile
