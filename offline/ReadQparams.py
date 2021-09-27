f188f3f338772a3b0eee964eba5b27ab71f6d88f
Nikki Tebaldi <ntebaldi@umass.edu>
2021-09-27 09:26:00 -0400

    Data product run type, SAD, and Abar - Constrained versus unconstrained data product run - SAD results - Abar numpy array

--- offline/ReadQparams.py
+++ offline/ReadQparams.py
@@ -11,34 +11,42 @@ from netCDF4 import Dataset
 import numpy as np
 
 
-def extract_alg(alg_dir, r_id):
+def extract_alg(alg_dir, r_id, run_type):
     """Extracts and stores reach-level FLPE algorithm data in alg_dict.
-    TODO: 
-    - Add in SAD results once available.
+    Parameters
+    ----------
+    alg_dir: Path
+        path to FLPE algorithm directory
+    r_id: int
+        unique reach identifier
+    run_type: str
+        constrained or unconstrained data product indicator
     """
     
-    gb_file = Path(alg_dir + '/geobam/' + f'{r_id}_geobam.nc')
-    hv_file = Path(alg_dir + '/hivdi/' + f'{r_id}_hivdi.nc')
-    mo_file = Path(alg_dir + '/momma/' + f'{r_id}_momma.nc')
-    sd_file = Path(alg_dir + '/sad/' + f'{r_id}_sad.nc')      ## TODO wait on SAD results
-    mm_file = glob(str(alg_dir + '/metroman/' + f'*{r_id}*_metroman.nc'))    
+    gb_file = alg_dir / 'geobam' / f'{r_id}_geobam.nc'
+    hv_file = alg_dir / 'hivdi' / f'{r_id}_hivdi.nc'
+    mo_file = alg_dir / 'momma' / f'{r_id}_momma.nc'
+    sd_file = alg_dir / 'sad' / f'{r_id}_sad.nc'
+    
+    mm_file = glob(str(alg_dir / 'metroman' / f'*{r_id}*_metroman.nc'))    
     mm_file = Path(mm_file[0]) 
 
-    if gb_file.exists() and hv_file.exists() and mm_file.exists() and mo_file.exists():    ## TODO add in SAD files
-
-        param_dict = extract_valid(r_id, gb_file, hv_file, mo_file, sd_file, mm_file)
+    if gb_file.exists() and hv_file.exists() and mm_file.exists() and mo_file.exists() and sd_file.exists():
+        param_dict = extract_valid(r_id, run_type, gb_file, hv_file, mo_file, sd_file, mm_file)
     else:
         param_dict = indicate_no_data(r_id)
         
     return param_dict
 
 
-def extract_valid(r_id, gb_file, hv_file, mo_file, sd_file, mm_file):
+def extract_valid(r_id, run_type, gb_file, hv_file, mo_file, sd_file, mm_file):
     """ Extract valid data from the output of each reach-level FLPE alg.
     Parameters
     ----------
     r_id: str
         Unique reach identifier
+    run_type: str
+        constrained or unconstrained data product indicator
     gb_file: Path
         Path to geoBAM results file
     hv_file: Path
@@ -50,43 +58,51 @@ def extract_valid(r_id, gb_file, hv_file, mo_file, sd_file, mm_file):
     mm_file: Path
         Path to MetroMan results file
     """
+
+    non_run_type = "constrained" if run_type == "unconstrained" else "unconstrained"
+    non_run_array = np.array([-9999], dtype=float)
    
     # note constrained/unconstrained not implemented yet
-    alg_dict = {'discharge_models': 
-                             {'unconstrained':{'MetroMan':{}, 
-                                               'BAM':{}, 
-                                               'HiVDI':{}, 
-                                               'MOMMA':{},
-                                               'SADS':{}}, 
-                              'constrained':{'MetroMan':{}, 
-                                             'BAM':{}, 
-                                             'HiVDI':{}, 
-                                             'MOMMA':{},
-                                             'SADS':{}} }}
+    alg_dict = {
+        'unconstrained':{
+            'MetroMan':{}, 
+            'BAM':{}, 
+            'HiVDI':{}, 
+            'MOMMA':{},
+            'SADS':{}
+        }, 
+        'constrained':{
+            'MetroMan':{}, 
+            'BAM':{}, 
+            'HiVDI':{}, 
+            'MOMMA':{},
+            'SADS':{}
+        } 
+    }
 
     # geobam
     gb = Dataset(gb_file, 'r', format="NETCDF4")
-    alg_dict['discharge_models']['unconstrained']['BAM'] = {
+    alg_dict[run_type]['BAM'] = {
         "n": np.nanmean(np.array(get_gb_data(gb, "logn_man", True))),
-        "Abar": np.nanmean(np.array(get_gb_data(gb, "A0", False)))
+        "Abar": np.array(np.nanmean(np.array(get_gb_data(gb, "A0", False))))
     }
-    alg_dict['discharge_models']['constrained']['BAM'] = {
-        "n": np.nanmean(np.array(get_gb_data(gb, "logn_man", True))),
-        "Abar": np.nanmean(np.array(get_gb_data(gb, "A0", False)))
+    alg_dict[non_run_type]['BAM'] = {
+        "n": non_run_array,
+        "Abar": non_run_array
     }
     gb.close()
 
     # hivdi
     hv = Dataset(hv_file, 'r', format="NETCDF4")
-    alg_dict['discharge_models']['unconstrained']['HiVDI'] = {
+    alg_dict[run_type]['HiVDI'] = {
         "alpha" : hv["reach"]["alpha"][:].filled(np.nan),  
         "beta" : hv["reach"]["beta"][:].filled(np.nan),  
-        "Abar" : hv["reach"]["A0"][:].filled(np.nan)
+        "Abar" : np.array(hv["reach"]["A0"][:].filled(np.nan))
     }
-    alg_dict['discharge_models']['constrained']['HiVDI'] = {
-        "alpha" : hv["reach"]["alpha"][:].filled(np.nan),  
-        "beta" : hv["reach"]["beta"][:].filled(np.nan),  
-        "Abar" : hv["reach"]["A0"][:].filled(np.nan)
+    alg_dict[non_run_type]['HiVDI'] = {
+        "alpha" : non_run_array,  
+        "beta" : non_run_array,  
+        "Abar" : non_run_array
     }
     hv.close()
 
@@ -94,47 +110,42 @@ def extract_valid(r_id, gb_file, hv_file, mo_file, sd_file, mm_file):
     mo = Dataset(mo_file, 'r', format="NETCDF4")
     with warnings.catch_warnings():
         warnings.simplefilter("ignore", category=RuntimeWarning)
-        alg_dict['discharge_models']['unconstrained']['MOMMA'] = {
+        alg_dict[run_type]['MOMMA'] = {
             "B" : mo["zero_flow_stage"][:].filled(np.nan),
             "H" : mo["bankfull_stage"][:].filled(np.nan),                                  
             "Save" : np.nanmean(mo["slope"][:].filled(np.nan))
         }
-        alg_dict['discharge_models']['constrained']['MOMMA'] = {
-            "B" : mo["zero_flow_stage"][:].filled(np.nan),
-            "H" : mo["bankfull_stage"][:].filled(np.nan),                                  
-            "Save" : np.nanmean(mo["slope"][:].filled(np.nan))
+        alg_dict[non_run_type]['MOMMA'] = {
+            "B" : non_run_array,
+            "H" : non_run_array,                                  
+            "Save" : non_run_array
         }
     mo.close()
 
-    # sad ugly way to make discharge.py run
-    # sd = Dataset(sd_file, 'r', format="NETCDF4")
-    # alg_dict['discharge_models']['unconstrained']['SADS'] = {
-    #     "n" : sd["n"][:].filled(np.nan),
-    #     "Abar" : sd["A0"][:].filled(np.nan)
-    # }
-    # sd.close()
-    alg_dict['discharge_models']['unconstrained']['SADS'] = {
-        "n" : np.nan,
-        "Abar" : np.nan
+    # sad
+    sd = Dataset(sd_file, 'r', format="NETCDF4")
+    alg_dict[run_type]['SADS'] = {
+        "n" : sd["n"][:].filled(np.nan),
+        "Abar" : np.array(sd["A0"][:].filled(np.nan))
     }
-    alg_dict['discharge_models']['constrained']['SADS'] = {
-        "n" : np.nan,
-        "Abar" : np.nan
+    alg_dict[non_run_type]['SADS'] = {
+        "n" : non_run_array,
+        "Abar" : non_run_array
     }
-    
+    sd.close()    
     
     # metroman    
     mm = Dataset(mm_file, 'r', format="NETCDF4")
     index = np.where(mm["reach_id"][:] == int(r_id))
-    alg_dict['discharge_models']['unconstrained']['MetroMan'] = {
+    alg_dict[run_type]['MetroMan'] = {
          "ninf" : mm["nahat"][index].filled(np.nan),
          "p" : mm["x1hat"][index].filled(np.nan),
          "Abar" : mm["A0hat"][index].filled(np.nan)
     }
-    alg_dict['discharge_models']['constrained']['MetroMan'] = {
-         "ninf" : mm["nahat"][index].filled(np.nan),
-         "p" : mm["x1hat"][index].filled(np.nan),
-         "Abar" : mm["A0hat"][index].filled(np.nan)
+    alg_dict[non_run_type]['MetroMan'] = {
+         "ninf" : non_run_array,
+         "p" : non_run_array,
+         "Abar" : non_run_array
     }
     mm.close()
     
