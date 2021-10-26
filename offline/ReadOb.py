fd87ad337e94cd8d7d6336e93e0359cf72278608
Nikki Tebaldi <ntebaldi@umass.edu>
2021-10-26 10:44:27 -0400

    Added slope, width, d_x_area uncertainty

--- offline/ReadOb.py
+++ offline/ReadOb.py
@@ -11,8 +11,11 @@ def Rivertile(swot_file):
     rivertile['reach_id'] = dataset['reach']['reach_id'][:].filled(np.nan)
     rivertile['height'] = dataset['reach']['wse'][:].filled(np.nan)
     rivertile['width'] = dataset['reach']['width'][:].filled(np.nan)
+    rivertile['width_u'] = dataset['reach']['width_u'][:].filled(np.nan)
     rivertile['slope'] = dataset['reach']['slope2'][:].filled(np.nan)
+    rivertile['slope_u'] = dataset['reach']['slope2_u'][:].filled(np.nan)
     rivertile['d_x_area'] = dataset['reach']['d_x_area'][:].filled(np.nan)
+    rivertile['d_x_area_u'] = dataset['reach']['d_x_area_u'][:].filled(np.nan)
     rivertile['nt'] = dataset.dimensions["nt"].size
     rivertile["time_steps"] = dataset["nt"][:]
  
