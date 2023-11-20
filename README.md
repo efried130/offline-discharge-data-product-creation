f1ffeac6da7e88e0d361ae8c9f88c5d9d9bfd8fa
Nikki Tebaldi <17799906+nikki-t@users.noreply.github.com>
2023-11-20 16:39:19 -0500

    Script to deploy container image and job definition

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # offline-discharge-data-product-creation
 Offline Discharge Data Product Creation
 
-**Example Usage**
+***Example Usage***
 
-```
+```bash
 InputDir=Path('./offline_inputs')
 reaches_json=InputDir.joinpath('reaches.json')
 
@@ -16,8 +16,33 @@ for reach in range(nR):
      %run /Users/mtd/GitHub/SWOT-confluence/offline-discharge-data-product-creation/run_offline.py 'unconstrained' 'timeseries' 'reaches.json'  $reach
      
 ```
-     
+
 So the command line arguments in order are [branch] [SWOT observation mode] [reaches file] [reach #]
 
 where [branch] is either "constrained" or "unconstrained"
 and [SWOT observation mode] is either "timeseries" or "single_pass". The "timeseries" option uses the .nc files created by the input module, and the "single_pass" uses the L2 single pass SWOT shapefiles.
+
+## deployment
+
+There is a script to deploy the Docker container image and Terraform AWS infrastructure found in the `deploy` directory.
+
+Script to deploy Terraform and Docker image AWS infrastructure
+
+REQUIRES:
+
+- jq (<https://jqlang.github.io/jq/>)
+- docker (<https://docs.docker.com/desktop/>) > version Docker 1.5
+- AWS CLI (<https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html>)
+- Terraform (<https://developer.hashicorp.com/terraform/tutorials/aws-get-started/install-cli>)
+
+Command line arguments:
+
+[1] registry: Registry URI
+[2] repository: Name of repository to create
+[3] prefix: Prefix to use for AWS resources associated with environment deploying to
+[4] s3_state_bucket: Name of the S3 bucket to store Terraform state in (no need for s3:// prefix)
+[5] profile: Name of profile used to authenticate AWS CLI commands
+
+Example usage: ``./deploy.sh "account-id.dkr.ecr.region.amazonaws.com" "container-image-name" "prefix-for-environment" "s3-state-bucket-name" "confluence-named-profile"`
+
+Note: Run the script from the deploy directory.
---
+++ deploy/terraform/confluence-offline.tf
@@ -0,0 +1,68 @@
+# Job Definition
+resource "aws_batch_job_definition" "generate_batch_jd_offline" {
+  name                  = "${var.prefix}-offline"
+  type                  = "container"
+  container_properties  = <<CONTAINER_PROPERTIES
+  {
+    "image": "${local.account_id}.dkr.ecr.us-west-2.amazonaws.com/offline",
+    "executionRoleArn": "${data.aws_iam_role.exe_role.arn}",
+    "jobRoleArn": "${data.aws_iam_role.job_role.arn}",
+    "fargatePlatformConfiguration": { "platformVersion": "LATEST" },
+    "logConfiguration": {
+      "logDriver" : "awslogs",
+      "options": {
+        "awslogs-group" : "${data.aws_cloudwatch_log_group.cw_log_group.name}"
+      }
+    },
+    "resourceRequirements": [
+      {"type": "MEMORY", "value": "2048"},
+      {"type": "VCPU", "value": "1"}
+    ],
+    "mountPoints": [
+      {
+        "sourceVolume": "input",
+        "containerPath": "/mnt/data/input",
+        "readOnly": true
+      },
+      {
+        "sourceVolume": "flpe",
+        "containerPath": "/mnt/data/flpe",
+        "readOnly": true
+      },
+      {
+        "sourceVolume": "offline",
+        "containerPath": "/mnt/data/output",
+        "readOnly": false
+      }
+    ],
+    "volumes": [
+      {
+        "name": "input",
+        "efsVolumeConfiguration": {
+          "fileSystemId": "${data.aws_efs_file_system.aws_efs_input.file_system_id}",
+          "rootDirectory": "/"
+        }
+      },
+      {
+        "name": "flpe",
+        "efsVolumeConfiguration": {
+          "fileSystemId": "${data.aws_efs_file_system.aws_efs_flpe.file_system_id}",
+          "rootDirectory": "/"
+        }
+      },
+      {
+        "name": "offline",
+        "efsVolumeConfiguration": {
+          "fileSystemId": "${data.aws_efs_file_system.aws_efs_offline.file_system_id}",
+          "rootDirectory": "/"
+        }
+      }
+    ]
+  }
+  CONTAINER_PROPERTIES
+  platform_capabilities = ["FARGATE"]
+  propagate_tags        = true
+   retry_strategy {
+    attempts = 3
+  }
+}
