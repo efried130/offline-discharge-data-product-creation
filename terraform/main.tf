e656a6d14c92f26798468237df547d8fd0813952
Josh Garde <jgarde@jpl.nasa.gov>
2025-05-20 06:19:55 -0700

    Parameterize Terraform Child Module (#8)
    * Initial parameterization refactor
    
    * Fix references to EFS and IAM roles in module
    
    ---------
    
    Co-authored-by: Nikki <17799906+nikki-t@users.noreply.github.com>

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,6 +19,30 @@ provider "aws" {
 
 data "aws_caller_identity" "current" {}
 
+data "aws_efs_file_system" "input" {
+  creation_token = "${var.prefix}-input"
+}
+
+data "aws_efs_file_system" "flpe" {
+  creation_token = "${var.prefix}-flpe"
+}
+
+data "aws_efs_file_system" "moi" {
+  creation_token = "${var.prefix}-moi"
+}
+
+data "aws_efs_file_system" "offline" {
+  creation_token = "${var.prefix}-offline"
+}
+
+data "aws_iam_role" "job" {
+  name = "${var.prefix}-batch-job-role"
+}
+
+data "aws_iam_role" "execution" {
+  name = "${var.prefix}-ecs-exe-task-role"
+}
+
 locals {
   account_id = sensitive(data.aws_caller_identity.current.account_id)
   default_tags = length(var.default_tags) == 0 ? {
@@ -29,10 +53,18 @@ locals {
 }
 
 module "confluence-offline" {
-  source            = "./modules/offline"
-  app_name          = var.app_name
-  app_version       = var.app_version
-  aws_region        = var.aws_region
-  environment       = var.environment
-  prefix            = var.prefix
+  source = "./modules/offline"
+  app_name = var.app_name
+  app_version = var.app_version
+  aws_region = var.aws_region
+  efs_file_system_ids = {
+    input = data.aws_efs_file_system.input.file_system_id
+    flpe = data.aws_efs_file_system.flpe.file_system_id
+    moi = data.aws_efs_file_system.moi.file_system_id
+    offline = data.aws_efs_file_system.offline.file_system_id
+  }
+  environment = var.environment
+  iam_execution_role_arn = data.aws_iam_role.execution.arn
+  iam_job_role_arn = data.aws_iam_role.job.arn
+  prefix = var.prefix
 }
--- terraform/modules/offline/confluence-offline.tf
+++ terraform/modules/offline/confluence-offline.tf
@@ -1,78 +1,77 @@
 # Job Definition
 resource "aws_batch_job_definition" "generate_batch_jd_offline" {
-  name                  = "${var.prefix}-offline"
-  type                  = "container"
-  container_properties  = <<CONTAINER_PROPERTIES
-  {
-    "image": "${local.account_id}.dkr.ecr.us-west-2.amazonaws.com/${var.prefix}-offline",
-    "executionRoleArn": "${data.aws_iam_role.exe_role.arn}",
-    "jobRoleArn": "${data.aws_iam_role.job_role.arn}",
-    "fargatePlatformConfiguration": { "platformVersion": "LATEST" },
-    "logConfiguration": {
-      "logDriver" : "awslogs",
-      "options": {
-        "awslogs-group" : "${data.aws_cloudwatch_log_group.cw_log_group.name}"
-      }
-    },
-    "resourceRequirements": [
-      {"type": "MEMORY", "value": "2048"},
-      {"type": "VCPU", "value": "1"}
-    ],
-    "mountPoints": [
-      {
-        "sourceVolume": "input",
-        "containerPath": "/mnt/data/input",
-        "readOnly": true
-      },
-      {
-        "sourceVolume": "flpe",
-        "containerPath": "/mnt/data/flpe",
-        "readOnly": true
-      },
-      {
-        "sourceVolume": "moi",
-        "containerPath": "/mnt/data/moi",
-        "readOnly": true
-      },
-      {
-        "sourceVolume": "offline",
-        "containerPath": "/mnt/data/output",
-        "readOnly": false
-      }
-    ],
-    "volumes": [
-      {
-        "name": "input",
-        "efsVolumeConfiguration": {
-          "fileSystemId": "${data.aws_efs_file_system.aws_efs_input.file_system_id}",
-          "rootDirectory": "/"
-        }
-      },
-      {
-        "name": "flpe",
-        "efsVolumeConfiguration": {
-          "fileSystemId": "${data.aws_efs_file_system.aws_efs_flpe.file_system_id}",
-          "rootDirectory": "/"
-        }
-      },
-      {
-        "name": "moi",
-        "efsVolumeConfiguration": {
-          "fileSystemId": "${data.aws_efs_file_system.aws_efs_moi.file_system_id}",
-          "rootDirectory": "/"
-        }
-      },
-      {
-        "name": "offline",
-        "efsVolumeConfiguration": {
-          "fileSystemId": "${data.aws_efs_file_system.aws_efs_offline.file_system_id}",
-          "rootDirectory": "/"
-        }
-      }
-    ]
-  }
-  CONTAINER_PROPERTIES
+  name = "${var.prefix}-offline"
+  type = "container"
   platform_capabilities = ["FARGATE"]
-  propagate_tags        = true
+  propagate_tags = true
   tags = { "job_definition": "${var.prefix}-offline" }
+
+  container_properties = jsonencode({
+    image = "${local.account_id}.dkr.ecr.us-west-2.amazonaws.com/${var.prefix}-offline:${var.image_tag}"
+    executionRoleArn = var.iam_execution_role_arn
+    jobRoleArn = var.iam_job_role_arn
+    fargatePlatformConfiguration = {
+      platformVersion = "LATEST"
+    }
+    logConfiguration = {
+      logDriver = "awslogs"
+      options = {
+        awslogs-group = aws_cloudwatch_log_group.cw_log_group.name
+      }
+    }
+    resourceRequirements = [{
+      type = "MEMORY"
+      value = "2048"
+    }, {
+      type = "VCPU",
+      value = "1"
+    }]
+    mountPoints = [{
+      sourceVolume = "input",
+      containerPath = "/mnt/data/input"
+      readOnly = false
+    }, {
+      sourceVolume = "flpe"
+      containerPath = "/mnt/data/flpe"
+      readOnly = false
+    }, {
+      sourceVolume = "moi"
+      containerPath = "/mnt/data/moi"
+      readOnly = false
+    }, {
+      sourceVolume = "offline"
+      containerPath = "/mnt/data/output"
+      readOnly = false
+    }]
+    volumes = [{
+      name = "input"
+      efsVolumeConfiguration = {
+        fileSystemId = var.efs_file_system_ids["input"]
+        rootDirectory = "/"
+      }
+    }, {
+      name = "flpe"
+      efsVolumeConfiguration = {
+        fileSystemId = var.efs_file_system_ids["flpe"]
+        rootDirectory = "/"
+      }
+    }, {
+      name = "moi"
+      efsVolumeConfiguration = {
+        fileSystemId = var.efs_file_system_ids["moi"]
+        rootDirectory = "/"
+      }
+    }, {
+      name = "offline"
+      efsVolumeConfiguration = {
+        fileSystemId = var.efs_file_system_ids["offline"]
+        rootDirectory = "/"
+      }
+    }]
+  })
+}
+
+# Log group
+resource "aws_cloudwatch_log_group" "cw_log_group" {
+  name = "/aws/batch/job/${var.prefix}-offline/"
 }
